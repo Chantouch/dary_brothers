9965d2bed5e54ae3df4a19bf6ea09f6d5333501e
Touch <s.chantouch@mango-byte.com>
2018-09-15 18:08:11 +0700

    Change config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,11 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
-
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .autoload({
+        jquery: ['$', 'window.jQuery', 'jQuery'],
+        select2: ['select2', 'window.select2']
+    });
 // Admin resources
 mix.js('resources/assets/js/admin.js', 'public/js')
     .sass('resources/assets/sass/admin.scss', 'public/css');
