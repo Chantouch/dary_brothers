964914bbb5b6758fd3971c6a8a838817f8e2b10e
Touch <s.chantouch@mango-byte.com>
2018-09-29 07:04:13 +0700

    queue contact email

---
+++ app/Jobs/ContactEmailSent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\SendContactEmail;
+use App\Mail\SendThanksContactEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class ContactEmailSent implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $user
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to(config('settings.app_email'))->send(new SendContactEmail($this->user));
+        Mail::to($this->user['email'])->send(new SendThanksContactEmail());
+    }
+}
