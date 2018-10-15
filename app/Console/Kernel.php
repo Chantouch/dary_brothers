5a0ba5cfd30cc9f40b5bcf9845776df593431a4c
Touch <s.chantouch@mango-byte.com>
2018-10-15 17:13:32 +0700

    Test schedule queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\EnsureQueueListenerIsRunning;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        EnsureQueueListenerIsRunning::class
     ];
 
     /**
@@ -26,8 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('queue:listen --tries=3')
-//            ->cron('* * * * *')
+        $schedule->command('queue:checkup')->everyMinute()
             ->withoutOverlapping();
     }
 
