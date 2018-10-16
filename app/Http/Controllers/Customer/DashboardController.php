4814af6a73fd829de7005a4e20bc969de8d0ce38
Touch <s.chantouch@mango-byte.com>
2018-10-16 23:04:34 +0700

    change cart content

--- app/Http/Controllers/Customer/DashboardController.php
+++ app/Http/Controllers/Customer/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        $productCarts = Cart::content();
+        $productCarts = Cart::instance('shopping')->content();
 
         // dd($productCarts);
 
