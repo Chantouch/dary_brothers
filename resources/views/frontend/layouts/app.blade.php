1342051ceb353b037c32d64053fa6cbcf46875c2
Touch <s.chantouch@mango-byte.com>
2018-10-27 16:00:40 +0700

    Testing

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -37,7 +37,7 @@
 
     @yield('style')
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-51288724-6"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-51288724-6" integrity="sha256-SLtuI24BkEZaoseVaRpnjIQW5BA2x5ckpMoTU0nD58w=" crossorigin="anonymous"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
@@ -89,26 +89,26 @@
 </div>
 </body>
 
-<script type="text/javascript" src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
 
 {{--<script type="text/javascript" src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>--}}
 {{--<script type="text/javascript" src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>--}}
 
-<script type="text/javascript" src="{{ asset('plugins/select2.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('plugins/select2.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('vendor/slick/slick.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/frontend/slick-custom.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/slick/slick.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('js/frontend/slick-custom.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('vendor/countdowntime/countdowntime.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/countdowntime/countdowntime.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('vendor/lightbox2/js/lightbox.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/lightbox2/js/lightbox.min.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
 
-<script type="text/javascript" src="{{ asset('vendor/parallax100/parallax100.js') }}"></script>
+<script async type="text/javascript" src="{{ asset('vendor/parallax100/parallax100.js') }}"></script>
 
 @include('sweet::alert')
 @yield('scripts')
