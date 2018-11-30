8c36b8c6139381adc54bdf93441c163595c2951d
Touch <s.chantouch@mango-byte.com>
2018-11-30 19:41:43 +0700

    update seting input

--- resources/views/admin/settings/fields.blade.php
+++ resources/views/admin/settings/fields.blade.php
@@ -29,7 +29,7 @@
         @elseif($json->type =='select_from_array')
             {!! Form::select('value', $json->options, null, ['class' => 'form-control']) !!}
         @else
-            {!! Form::text('value',null , ['class' => 'form-control', 'placeholder' => 'Enter value']) !!}
+            {!! Form::textarea('value', null, ['class' => 'form-control' . ($errors->has('value') ? ' is-invalid' : ''), 'placeholder' => __('settings.placeholder.description')]) !!}
         @endif
 
         @if ($errors->has('value'))
