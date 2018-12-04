1b86450a309a89f480002c369152c91cf804a9ee
Touch <s.chantouch@mango-byte.com>
2018-12-04 22:21:39 +0700

    minified resource

--- public/js/admin/product-editor.js
+++ public/js/admin/product-editor.js
@@ -1,12 +1,1 @@
-$('.editor').trumbowyg();
-
-$('.select2').select2();
-
-$('#image_uploads').filer({
-    limit: 5,
-    maxSize: 3,
-    extensions: ['jpg', 'jpeg', 'png', 'gif'],
-    changeInput: true,
-    showThumbs: true,
-    addMore: true
-});
+$(".editor").trumbowyg(),$(".select2").select2(),$("#image_uploads").filer({limit:5,maxSize:3,extensions:["jpg","jpeg","png","gif"],changeInput:!0,showThumbs:!0,addMore:!0});
